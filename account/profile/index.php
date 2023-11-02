dc1f058466b2275226f4fa7db213556909d70f08
Evangelos <contact@vagdedes.com>
2023-11-02 05:11:22 +0200

    Fixed incorrect requirement

--- account/profile/index.php
+++ account/profile/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '/var/www/.structure/library/design/account/loader.php';
+require_once '/var/www/.structure/library/account/api/tasks/loader.php';
 load_page(true, function (bool $isLoggedIn, Application $application) {
     global $website_account_url;
 
