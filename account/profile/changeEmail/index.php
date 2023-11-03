2c08ae3869830a0238f67be9b255442e615b7293
Evangelos <contact@vagdedes.com>
2023-11-04 00:48:04 +0200

    Made email verification not require log in

--- account/profile/changeEmail/index.php
+++ account/profile/changeEmail/index.php
@@ -1,35 +1,18 @@
 <?php
 require '/var/www/.structure/library/account/api/tasks/loader.php';
 load_page(true, function (Account $account, bool $isLoggedIn) {
-    if (!$isLoggedIn) {
-        $token = get_form_get("token");
+    $token = get_form_get("token");
 
-        if (!empty($token)) {
-            echo "<div class='area'>
-                    <div class='area_form'>
-                        <form method='post'>
-                            <input type='email' name='email' placeholder='New Email Address' minlength=0 maxlength=0>
-                            <input type='submit' name='change' value='You Must Be Logged In' class='button' id='blue'>
-                        </form>
-                    </div>
-                </div>";
-        } else {
-            account_page_redirect(null, false, null);
+    if (!empty($token)) {
+        $result = $account->getEmail()->completeVerification($token);
+        account_page_redirect($account, true, $result->getMessage());
+    } else if ($isLoggedIn && isset($_POST["change"])) {
+        $result = $account->getEmail()->requestVerification(get_form_post("email"));
+        $result = $result->getMessage();
+
+        if (!empty($result)) {
+            $account->getNotifications()->add(AccountNotifications::FORM, "green", $result, "1 minute");
         }
-    } else {
-        $token = get_form_get("token");
-
-        if (!empty($token)) {
-            $result = $account->getEmail()->completeVerification($token);
-            account_page_redirect($account, true, $result->getMessage());
-        } else if (isset($_POST["change"])) {
-            $result = $account->getEmail()->requestVerification(get_form_post("email"));
-            $result = $result->getMessage();
-
-            if (!empty($result)) {
-                $account->getNotifications()->add(AccountNotifications::FORM, "green", $result, "1 minute");
-            }
-        }
-        account_page_redirect(null, false, null);
     }
+    account_page_redirect($account, $isLoggedIn, null);
 });
