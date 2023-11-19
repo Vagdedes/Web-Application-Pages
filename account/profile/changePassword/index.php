df3c80db2e05d0fb09c4af1d3e26cb6ea054c43b
Evangelos <contact@vagdedes.com>
2023-11-19 21:01:34 +0200

    Moved part of reset password form to discord bot

--- account/profile/changePassword/index.php
+++ account/profile/changePassword/index.php
@@ -34,47 +34,7 @@ load_page(true, function (Account $account, bool $isLoggedIn) {
                     </form>
                 </div>
             </div>";
-    } else if ($isLoggedIn) {
-        account_page_redirect($account, true, null);
     } else {
-        if (isset($_POST["change"])) {
-            if (!is_google_captcha_valid()) {
-                redirect_to_url("?message=Please complete the bot verification");
-            } else {
-                $email = get_form_post("email");
-
-                if (!is_email($email)) {
-                    redirect_to_url("?message=Please enter a valid email address");
-                } else {
-                    $account = $account->getNew(null, $email);
-
-                    if ($account->exists()) {
-                        $result = $account->getPassword()->requestChange();
-                        $result = $result->getMessage();
-
-                        if (!empty($result)) {
-                            account_page_redirect(null, false, $result);
-                        } else {
-                            account_page_redirect(null, false, null);
-                        }
-                    } else {
-                        redirect_to_url("?message=Account with this email address does not exist");
-                    }
-                }
-            }
-        }
-
-        echo "<div class='area'>
-                <div class='area_form'>
-                    <form method='post'>
-                        <input type='email' name='email' placeholder='Email Address' minlength=5 maxlength=384>
-                        <input type='submit' name='change' value='Request Change Password' class='button' id='blue'>
-
-                         <div class=recaptcha>
-    		                <div class=g-recaptcha data-sitekey=6Lf_zyQUAAAAAAxfpHY5Io2l23ay3lSWgRzi_l6B></div>
-    		            </div>
-                    </form>
-                </div>
-            </div>";
+        account_page_redirect($account, true, null);
     }
 });
