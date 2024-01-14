7293dc6216543c2621b0bd5e99b202e77e4f39cd
Evangelos <contact@vagdedes.com>
2024-01-14 18:49:59 +0200

    Partly unified communication form

--- account/profile/contact/index.php
+++ account/profile/contact/index.php
@@ -18,33 +18,9 @@ load_page(true, function (Account $account, bool $isLoggedIn) {
             } else if (has_memory_cooldown($cacheKey, null, false)) {
                 account_page_redirect($account, $isLoggedIn, "Please wait a few minutes before contacting us again.", false);
             } else {
-                $platformsString = null;
+                $content = $account->getEmail()->getSupportEmailDetails($isLoggedIn, $subject, $info, $emailForm);
 
-                if ($isLoggedIn) {
-                    $accounts = $account->getAccounts()->getAdded();
-
-                    if (!empty($accounts)) {
-                        $platformsString = "Accounts:\r\n";
-
-                        foreach ($accounts as $row) {
-                            $platformsString .= $row->accepted_account->name . ": " . $row->credential . "\r\n";
-                        }
-                        $platformsString .= "\r\n";
-                    }
-                }
-                $id = rand(0, 2147483647);
-                $email = $isLoggedIn ? $account->getDetail("email_address") : $emailForm;
-                $subject = strip_tags($subject);
-                $title = get_domain() . " - $subject [ID: $id]";
-                $content = "ID: $id" . "\r\n"
-                    . "Subject: $subject" . "\r\n"
-                    . "Email: $email" . "\r\n"
-                    . "Type: " . ($isLoggedIn ? "Logged In" : "Logged Out")
-                    . "\r\n"
-                    . ($platformsString !== null ? $platformsString : "\r\n")
-                    . strip_tags($info);
-
-                if (services_self_email($email, $title, $content)) {
+                if (services_self_email($content[0], $content[1], $content[2])) {
                     has_memory_cooldown($cacheKey, "5 minutes");
                     account_page_redirect($account, $isLoggedIn, "Thanks for taking the time to contact us.");
                 } else {
@@ -86,7 +62,7 @@ load_page(true, function (Account $account, bool $isLoggedIn) {
         echo "<input type='email' name='email' placeholder='Email Address' minlength=5 maxlength=384>";
     }
     echo "<input type='text' name='subject' placeholder='Subject' minlength=2 maxlength=64>
-                        <textarea name='info' placeholder='Information regarding contact...' minlength=24 maxlength=512
+                        <textarea name='info' placeholder='Information regarding contact...' minlength=24 maxlength=2048
                                   style='height: 150px; min-height: 150px;'></textarea>
                         <input type='submit' name='contact' value='Contact Us' class='button' id='blue'>";
 
