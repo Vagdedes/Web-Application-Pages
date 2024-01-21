ff1d5189aa78a71b2de9eb5c7a459037b9aee16a
Evangelos <contact@vagdedes.com>
2024-01-22 01:11:49 +0200

    Reduced object parameters

--- account/profile/instantLogin/index.php
+++ account/profile/instantLogin/index.php
@@ -1,10 +1,10 @@
 <?php
 require '/var/www/.structure/library/account/api/tasks/loader.php';
-load_page(false, function (Account $account, bool $isLoggedIn, AccountSession $session) {
+load_page(false, function (Account $account, bool $isLoggedIn) {
     if ($isLoggedIn) {
         account_page_redirect($account, true, null);
     } else {
-        $twoFactor = $session->getTwoFactorAuthentication();
+        $twoFactor = $account->getTwoFactorAuthentication();
         $twoFactor = $twoFactor->verify(get_form_get("token"));
         account_page_redirect($twoFactor->getObject(), $twoFactor->isPositiveOutcome(), $twoFactor->getMessage());
     }
