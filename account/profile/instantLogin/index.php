c2763da53fab00ca39198bac25af9b0a51dd8c1f
Evangelos <contact@vagdedes.com>
2024-02-05 08:32:29 +0200

    Fixed two factor authentication issue

--- account/profile/instantLogin/index.php
+++ account/profile/instantLogin/index.php
@@ -2,8 +2,8 @@
 require '/var/www/.structure/library/account/api/tasks/loader.php';
 load_account_page(false, function (Account $account) {
     if ($account->exists()) {
-        echo json_encode($account->getTwoFactorAuthentication()->verify(get_form_get("token"))->getMessage());
+        echo json_encode("Your account is already verified.");
     } else {
-        echo json_encode("No account found.");
+        echo json_encode($account->getTwoFactorAuthentication()->verify(get_form_get("token"))->getMessage());
     }
 });
