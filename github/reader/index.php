41262fc45ed4e0be2cc045b21edeaa347ad688d8
Evangelos <contact@vagdedes.com>
2024-02-22 04:04:10 +0200

    Updated github reader

--- github/reader/index.php
+++ github/reader/index.php
@@ -11,19 +11,19 @@ $cacheKey = array(
 );
 $file = get_key_value_pair($cacheKey);
 
-if ($file === null || $file === false) {
+if ($file === null) {
     $file = @file_get_contents("https://github.com/IdealisticAI/"
         . ($_GET["repo"] ?? ".github")
         . "/blob/main/"
         . ($_GET["path"] ?? "README") . ".md");
     set_key_value_pair($cacheKey, $file, "5 minutes");
-
-    if ($file === false) {
-        header("Location: " . $website);
-        exit();
-    }
 }
-echo $file;
+if ($file === false) {
+    header("Location: " . $website);
+    exit();
+} else {
+    echo $file;
+}
 ?>
 <script type="text/javascript">
     window.onload = function () {
