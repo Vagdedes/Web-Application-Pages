dd5c9515b9c45b19d5e8fc9562b7c8e4ac27d873
Evangelos <vaggdedes@gmail.com>
2025-06-19 18:02:14 +0300

    Added txt headers

--- bigmanage/policies/privacy_policy/index.php
+++ bigmanage/policies/privacy_policy/index.php
@@ -1,2 +1,3 @@
 <?php
+header('Content-type: text/plain');
 echo @file_get_contents("https://raw.githubusercontent.com/IdealisticAI/Legal-Information/refs/heads/main/policies/privacy_policy.txt");
--- bigmanage/terms/terms_of_service/index.php
+++ bigmanage/terms/terms_of_service/index.php
@@ -1,2 +1,3 @@
 <?php
+header('Content-type: text/plain');
 echo @file_get_contents("https://raw.githubusercontent.com/IdealisticAI/Legal-Information/refs/heads/main/terms/terms_of_service.txt");
