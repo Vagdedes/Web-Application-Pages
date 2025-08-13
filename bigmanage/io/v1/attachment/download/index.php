b590c33227ba1fdb1bc19f3db795d87cc1b59c34
Evangelos <vaggdedes@gmail.com>
2025-08-13 16:12:28 +0300

    Adjusted the links

---
+++ bigmanage/io/v1/attachment/download/index.php
@@ -0,0 +1,2 @@
+<?php
+require '/var/www/.structure/library/bigmanage/api/tasks/input_output/v1/attachment/download/index.php';
--- bigmanage/io/v1/attachment/index.php
+++ bigmanage/io/v1/attachment/index.php
@@ -1,2 +1,2 @@
 <?php
-require '/var/www/.structure/library/bigmanage/api/tasks/input_output/v1/attachment/index.php';
+require '/var/www/.structure/library/base/redirect.php';
