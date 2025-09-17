624d4c87f2b0473e4baf8227a79e45d99a6d686c
Evangelos <vaggdedes@gmail.com>
2025-09-17 08:42:36 +0300

    added language paths

---
+++ it/index.php
@@ -0,0 +1,3 @@
+<?php
+$lang = "italian";
+include '/var/www/.structure/library/bigmanage/api/tasks/website/v1/index.php';
---
+++ pt/index.php
@@ -0,0 +1,3 @@
+<?php
+$lang = "portuguese";
+include '/var/www/.structure/library/bigmanage/api/tasks/website/v1/index.php';
