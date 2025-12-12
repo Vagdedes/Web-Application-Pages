5e3b8cb50d5a51993b4fc9fc214c54ef8ec5d3a1
Evangelos <vaggdedes@gmail.com>
2025-12-12 15:15:37 +0200

    Create index.php

---
+++ bigmanage/documentation/visual/index.php
@@ -0,0 +1,4 @@
+<?php
+require '/var/www/.structure/library/base/utilities.php';
+redirect_to_url("https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vQNjyPgD6kzAr3-ffCU2uhBUKsiWfCfipItaPgNCahHvz-ChgozZNWJ_nJOAi-DhsOYWaG5WxSlPZ5L/pubhtml");
+exit();
