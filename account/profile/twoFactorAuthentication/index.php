f1a928b99ef1299c9848450d5389826cd84d701e
Evangelos <vaggdedes@gmail.com>
2025-12-23 06:03:42 +0200

    Update index.php

--- account/profile/twoFactorAuthentication/index.php
+++ account/profile/twoFactorAuthentication/index.php
@@ -3,9 +3,7 @@ require '/var/www/.structure/library/account/api/tasks/loader.php';
 load_account_page(function (Account $account) {
     if ($account->exists()) {
         echo json_encode("Your account is already verified.");
-    } else if ($account->getPermissions()->isAdministrator()) {
-        echo json_encode($account->getTwoFactorAuthentication()->verify(get_form_get("token"))->getMessage());
     } else {
-        echo json_encode("You must be an administrator to verify two-factor authentication.");
+        echo json_encode($account->getTwoFactorAuthentication()->verify(get_form_get("token"))->getMessage());
     }
 });
