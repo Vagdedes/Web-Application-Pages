2b61623d116cf206e1f1a9e4f9f2512af5e81815
Evangelos <vaggdedes@gmail.com>
2026-03-08 07:33:30 +0200

    Revert "Delete index.php"
    This reverts commit f99819b137cdfc1a8d3bb18aab7e36493e90c688.

---
+++ office/pricing/index.php
@@ -0,0 +1,4 @@
+<?php
+require '/var/www/.structure/library/base/utilities.php';
+redirect_to_url("https://docs.google.com/spreadsheets/d/e/2PACX-1vTsJZcN2UCAygiVSM6Odn3IwCfKHezsUerI8gxjlXbW9sorAFbNN2qEZcmXaOTy35FMccHarsUfQqlH/pubhtml");
+exit();
