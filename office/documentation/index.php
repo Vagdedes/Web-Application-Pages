363766441c9ac580f41215cdaecdec5e65ac7bb2
Evangelos <vaggdedes@gmail.com>
2026-03-11 19:27:57 +0200

    Update index.php

--- office/documentation/index.php
+++ office/documentation/index.php
@@ -1,4 +1,4 @@
 <?php
 require '/var/www/.structure/library/base/utilities.php';
-redirect_to_url("https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vRCyl4iItjTGc4WLlFqXYVza4uXoMUYRydGtfk1nqr0I3H6VoGdZUKqc1PjjyZEwhhXslf3Wz20Oba4/pubhtml#gid=1276312557");
+redirect_to_url("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCyl4iItjTGc4WLlFqXYVza4uXoMUYRydGtfk1nqr0I3H6VoGdZUKqc1PjjyZEwhhXslf3Wz20Oba4/pubhtml");
 exit();
